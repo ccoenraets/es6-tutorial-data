c7473f4a695f75a4fbc1f4a0f8365a688386a42c
ccoenraets <ccoenraets@gmail.com>
2016-06-27 17:24:20 -0400

    Step 2

--- js/app.js
+++ js/app.js
@@ -1,35 +1,40 @@
 // XMLHttpRequest wrapper using callbacks
-function request(obj, successHandler, errorHandler) {
-    var xhr = new XMLHttpRequest();
+let request = (obj, successHandler, errorHandler) => {
+    let xhr = new XMLHttpRequest();
     xhr.open(obj.method || "GET", obj.url);
     if (obj.headers) {
-        Object.keys(obj.headers).forEach(function(key) {
+        Object.keys(obj.headers).forEach(key => {
             xhr.setRequestHeader(key, obj.headers[key]);
-        });
+    });
     }
-    xhr.onload = function() {
+    xhr.onload = () => {
         if (xhr.status >= 200 && xhr.status < 300) {
             successHandler(xhr.response);
         } else {
             errorHandler(xhr.statusText);
         }
     };
-    xhr.onerror = function() {
-        errorHandler(xhr.statusText);
-    };
+    xhr.onerror = () => errorHandler(xhr.statusText);
     xhr.send(obj.body);
-}
+};
 
 request({url:"employees.json"},
-    function(data) {
-        var employees = JSON.parse(data);
-        var html = "";
-        employees.forEach(function(employee){
-            html += "<div><img src='" + employee.picture + "'/><div>" + employee.firstName + " " + employee.lastName + "<p>" + employee.phone + "</p></div></div>";
-        });
-        document.getElementById("list").innerHTML = html;
-    },
-    function(error) {
-        console.log(error);
-    }
+    data => {
+    let employees = JSON.parse(data);
+let html = "";
+employees.forEach(employee => {
+    html += `
+                <div>
+                    <img src='${employee.picture}'/>
+                    <div>
+                        ${employee.firstName} ${employee.lastName}
+                        <p>${employee.phone}</p>
+                    </div>
+                </div>`;
+});
+document.getElementById("list").innerHTML = html;
+},
+error => {
+    console.log(error);
+}
 );
