4e14138046fa083456f168a8cafa9b48c5d68e86
ccoenraets <ccoenraets@gmail.com>
2016-06-27 18:48:46 -0400

    Step 3

--- js/app.js
+++ js/app.js
@@ -1,25 +1,27 @@
 // XMLHttpRequest wrapper using callbacks
-let request = (obj, successHandler, errorHandler) => {
-    let xhr = new XMLHttpRequest();
-    xhr.open(obj.method || "GET", obj.url);
-    if (obj.headers) {
-        Object.keys(obj.headers).forEach(key => {
-            xhr.setRequestHeader(key, obj.headers[key]);
-        });
-    }
-    xhr.onload = () => {
-        if (xhr.status >= 200 && xhr.status < 300) {
-            successHandler(xhr.response);
-        } else {
-            errorHandler(xhr.statusText);
+let request = obj => {
+    return new Promise((resolve, reject) => {
+        let xhr = new XMLHttpRequest();
+        xhr.open(obj.method || "GET", obj.url);
+        if (obj.headers) {
+            Object.keys(obj.headers).forEach(key => {
+                xhr.setRequestHeader(key, obj.headers[key]);
+            });
         }
-    };
-    xhr.onerror = () => errorHandler(xhr.statusText);
-    xhr.send(obj.body);
+        xhr.onload = () => {
+            if (xhr.status >= 200 && xhr.status < 300) {
+                resolve(xhr.response);
+            } else {
+                reject(xhr.statusText);
+            }
+        };
+        xhr.onerror = () => reject(xhr.statusText);
+        xhr.send(obj.body);
+    });
 };
 
-request({url: "employees.json"},
-    data => {
+request({url: "employees.json"})
+    .then(data => {
         let employees = JSON.parse(data);
         let html = "";
         employees.forEach(employee => {
@@ -33,8 +35,7 @@ request({url: "employees.json"},
                 </div>`;
         });
         document.getElementById("list").innerHTML = html;
-    },
-    error => {
+    })
+    .catch(error => {
         console.log(error);
-    }
-);
+    });
